9a75da1afc13c78652cb63669d8edee9166a87ae
ktw09876 <dhqhfjem@gmail.com>
2023-08-23 18:51:51 +0900

    CRUD 구현 완료

--- src/main/java/com/study/domain/post/PostController.java
+++ src/main/java/com/study/domain/post/PostController.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller //controller임을 명시
 @RequiredArgsConstructor
@@ -14,16 +18,52 @@ public class PostController {
     // 게시글 작성 페이지
     //http://localhost:8080/post/write.do
     @GetMapping("/post/write.do")
-    public String openPostWrite(Model model) {
-        String title = "제목",
-               content = "내용",
-               writer = "홍길동";
-
-        model.addAttribute("t", title);
-        model.addAttribute("c", content);
-        model.addAttribute("w", writer);
-
+    public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model) { //@RequestParam 화면(HTML)에서 보낸 파라미터를 전달받는 데 사용됨, required 속성을 false로 지정하지 않으면, id가 파라미터로 넘어오지 않았을 때 예외가 발생한다
+        if (id != null) {
+            PostResponse post = postService.findPostById(id);
+            model.addAttribute("post", post);
+        }
         return "post/write";
     }
+
+    // 신규 게시글 생성
+    @PostMapping("/post/save.do")
+    public String savePost(final PostRequest params) {
+        postService.savePost(params);
+        return "redirect:/post/list.do";
+    }
+
+    // 게시글 리스트 페이지
+    @GetMapping("/post/list.do")
+    public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+        return "post/list";
+    }
+
+    // 게시글 상세 페이지
+    @GetMapping("/post/view.do")
+    public String openPostView(@RequestParam final Long id, Model model) {
+        PostResponse post = postService.findPostById(id);
+        model.addAttribute("post", post);
+        return "post/view";
+    }
+
+    // 기존 게시글 수정
+    @PostMapping("/post/update.do")
+    public String updatePost(final PostRequest params) {
+        postService.updatePost(params);
+        return "redirect:/post/list.do";
+    }
+
+    // 게시글 삭제
+    //물리적인 삭제가 아닌 삭제 여부(delete_yn)컬럼의 값을 변경하는 논리 삭제
+    @PostMapping("/post/delete.do")
+    public String deletePost(@RequestParam final Long id) {
+        postService.deletePost(id);
+        return "redirect:/post/list.do"; //삭제 후 리스트 페이지로
+    }
+
+
 
 }
--- src/main/java/com/study/domain/post/PostRequest.java
+++ src/main/java/com/study/domain/post/PostRequest.java
@@ -7,10 +7,11 @@ import lombok.Setter;
 @Setter
 public class PostRequest {
 
-    private Long id;             // PK
-    private String title;        // 제목
-    private String content;      // 내용
-    private String writer;       // 작성자
-    private Boolean noticeYn;    // 공지글 여부
+    //사용자로부터 입력 받는 필드의 "name"과 맴버 변수명이 동일해야 한다
+    private Long id; // PK
+    private String title; // 제목 
+    private String content; // 내용
+    private String writer; // 작성자
+    private Boolean noticeYn; // 공지글 여부
 
 }
