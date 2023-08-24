e580d735ca004b5d05c7f8ee39588419f9a92c26
ktw09876 <dhqhfjem@gmail.com>
2023-08-24 15:34:39 +0900

    CRUD alert 메세지 구현

--- src/main/java/com/study/domain/post/PostController.java
+++ src/main/java/com/study/domain/post/PostController.java
@@ -1,10 +1,12 @@
 package com.study.domain.post;
 
+import com.study.common.dto.MessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -26,11 +28,19 @@ public class PostController {
         return "post/write";
     }
 
+//    // 신규 게시글 생성
+//    @PostMapping("/post/save.do")
+//    public String savePost(final PostRequest params) {
+//        postService.savePost(params);
+//        return "redirect:/post/list.do";
+//    }
+
     // 신규 게시글 생성
     @PostMapping("/post/save.do")
-    public String savePost(final PostRequest params) {
+    public String savePost(final PostRequest params, Model model) {
         postService.savePost(params);
-        return "redirect:/post/list.do";
+        MessageDto message = new MessageDto("게시글 생성이 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        return showMessageAndRedirect(message, model);
     }
 
     // 게시글 리스트 페이지
@@ -49,19 +59,41 @@ public class PostController {
         return "post/view";
     }
 
+//    // 기존 게시글 수정
+//    @PostMapping("/post/update.do")
+//    public String updatePost(final PostRequest params) {
+//        postService.updatePost(params);
+//        return "redirect:/post/list.do";
+//    }
+
     // 기존 게시글 수정
     @PostMapping("/post/update.do")
-    public String updatePost(final PostRequest params) {
+    public String updatePost(final PostRequest params, Model model) {
         postService.updatePost(params);
-        return "redirect:/post/list.do";
+        MessageDto message = new MessageDto("게시글 수정이 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        return showMessageAndRedirect(message, model);
     }
 
+//    // 게시글 삭제
+//    //물리적인 삭제가 아닌 삭제 여부(delete_yn)컬럼의 값을 변경하는 논리 삭제
+//    @PostMapping("/post/delete.do")
+//    public String deletePost(@RequestParam final Long id) {
+//        postService.deletePost(id);
+//        return "redirect:/post/list.do"; //삭제 후 리스트 페이지로
+//    }
+
     // 게시글 삭제
-    //물리적인 삭제가 아닌 삭제 여부(delete_yn)컬럼의 값을 변경하는 논리 삭제
     @PostMapping("/post/delete.do")
-    public String deletePost(@RequestParam final Long id) {
+    public String deletePost(@RequestParam final Long id, Model model) {
         postService.deletePost(id);
-        return "redirect:/post/list.do"; //삭제 후 리스트 페이지로
+        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        return showMessageAndRedirect(message, model);
+    }
+
+    // 사용자에게 메시지를 전달하고, 페이지를 리다이렉트 한다.
+    private String showMessageAndRedirect(final MessageDto params, Model model) {
+        model.addAttribute("params", params);
+        return "common/messageRedirect";
     }
 
 
